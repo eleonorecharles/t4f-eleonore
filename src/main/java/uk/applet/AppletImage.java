07d79e1773888a2dce5724f8227c8acd27920cd5
Eric Charles <eric@aos.io>
2013-05-15 08:37:35 +0100

    Automatic message for commit of Wed 15 May 2013 07:37:35 UTC

---
+++ src/main/java/uk/applet/AppletImage.java
@@ -0,0 +1,26 @@
+package uk.applet;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class AppletImage extends Applet {
+    
+    public void paint (Graphics g ) {
+        
+        try {
+            Image image = ImageIO.read(AppletImage.class.getResourceAsStream("/vans.jpg")) ;
+            g.drawImage(image, 0, 0, this);
+            g.drawString("KABOUMMMM", 0, 200 ) ;
+            
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+}
