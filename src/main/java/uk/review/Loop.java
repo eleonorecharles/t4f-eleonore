309d91ecf140d0d4d010d4055d04985710d30c0d
Eric Charles <eric@aos.io>
2013-05-22 08:01:00 +0100

    Automatic message for commit of Wed 22 May 2013 07:01:00 UTC

---
+++ src/main/java/uk/review/Loop.java
@@ -0,0 +1,29 @@
+package uk.review;
+
+import aos.console.AosConsole;
+
+public class Loop {
+
+    public static void main(String[] args) {
+       
+        for ( Integer i = 0 ; i<10 ; i ++ ) {
+            
+            AosConsole.println( "Multiplication by " + i );
+            
+            for ( Integer j = 0 ; j<10 ; j++ ){
+                
+                AosConsole.println(i + "*" + j + "=" + i*j );
+                
+            }
+            
+            
+            AosConsole.println("--------------------");
+            
+               
+            
+            
+        }
+
+    }
+
+}
