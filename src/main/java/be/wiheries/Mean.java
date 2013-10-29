195d52fec7f56eca42b8f65f7edf0131595dd1b1
Eric Charles <eric@aos.io>
2013-10-29 19:46:37 +0100

    Automatic message for commit of Tue Oct 29 18:46:37 UTC 2013

---
+++ src/main/java/be/wiheries/Mean.java
@@ -0,0 +1,37 @@
+package be.wiheries;
+
+import java.util.Random;
+
+import aos.console.AosConsole;
+
+public class Mean {
+
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+        int n = random.nextInt(10) ;
+        
+        int sum = 0 ;
+        
+        for ( int i=0; i<n ; i++ ) {
+            
+            int f = random.nextInt(100) ;
+            
+            AosConsole.println(f);
+            
+            sum = sum+f ;
+         
+        }
+        
+        AosConsole.println("-------------------");
+        AosConsole.println("Sum = " +sum);
+        AosConsole.println("Mean = " +sum/n);
+        
+        
+              
+        
+
+
+    }
+
+}
---
+++ src/main/java/be/wiheries/Refresh.java
@@ -0,0 +1,29 @@
+package be.wiheries;
+
+public class Refresh {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Counting from 0 to 10 :");
+        for ( int i=0 ; i<10 ; i ++ ) {
+            System.out.println(i);
+        }
+        
+        System.out.println("Decounting from 10 to 0 by 2:");
+        for ( int i=10 ; i>0 ; i=i- 2 ) {
+            System.out.println(i);
+        }
+        
+            System.out.println("Decounting from 30 to 10 by 3:");
+            for ( int i=30 ; i>10 ; i= i- 3 ) {
+                System.out.println(i);
+            }
+
+    }
+    
+    
+}
+
+       
+        
+
