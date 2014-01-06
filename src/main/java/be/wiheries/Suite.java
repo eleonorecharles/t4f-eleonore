1b705345a2b64a7ce881ebad896287b5d4add45e
Eric Charles <eric@aos.io>
2014-01-06 19:04:35 +0100

    Automatic message for commit of Mon Jan  6 18:04:35 UTC 2014

---
+++ src/main/java/be/wiheries/Suite.java
@@ -0,0 +1,34 @@
+package be.wiheries;
+
+import io.aos.console.AosConsole;
+
+public class Suite {
+
+    public static void main(String[] args) {
+        
+        AosConsole.println("sum =" + sum ( 1, 5, 10 ));
+        
+    }
+
+    private static int sum ( int u1, int r, int n) {
+        
+        int total = 0 ;
+         int ui = u1 ;
+        for (int i = 0 ; i < n ; i++ ) {
+            
+        total += r ;
+        ui += r;
+       
+      
+        }
+
+      
+        
+        
+        
+        return total ;
+    }
+    
+    
+    
+}
